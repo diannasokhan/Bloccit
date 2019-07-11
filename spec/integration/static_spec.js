f56ed8cbd363516c6fb8a0506b5f1b8769ccbd0c
Dianna Sokhan <diannasokhan@gmail.com>
2019-07-10 21:40:14 -0400

    new route to marco

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -13,13 +13,13 @@ describe('routes : static', () => {
     });
     describe('GET /macro', () => {
         it('should return status code 200', (done) => {
-            request.get(base, (err, res, body) => {
+            request.get('http://localhost:3000/marco', (err, res, body) => {
                 expect(res.statusCode).toBe(200);
                 done()
             })
         })
         it('should contain "polo" in response', (done) => {
-            request.get(base, (err, res, body) => {
+            request.get('http://localhost:3000/marco', (err, res, body) => {
                 expect(res.body).toContain('polo');
                 done()
             })
--- src/routes/static.js
+++ src/routes/static.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('Welcome to Bloccit polo')
+    res.send('Welcome to Bloccit')
+});
+router.get('/marco', (req, res, next) => {
+    res.send('Marco polo')
 });
 module.exports = router;
