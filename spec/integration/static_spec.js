5712098934283d42f5ce9dc4348f3a1b34019269
Dianna Sokhan <diannasokhan@gmail.com>
2019-07-12 21:41:55 -0400

    adding views

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -4,25 +4,12 @@ const base = 'http://localhost:3000/';
 
 describe('routes : static', () => {
     describe('GET /', () => {
-        it('should return status code 200', (done) => {
+        it('should return status code 200 and have "Welcome to Bloccit" in the body of the response', (done) => {
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
+                expect(body).toContain('Welcome to Bloccit')
                 done();
             });
         });
-    });
-    describe('GET /macro', () => {
-        it('should return status code 200', (done) => {
-            request.get(base, (err, res, body) => {
-                expect(res.statusCode).toBe(200);
-                done()
-            })
-        })
-        it('should contain "polo" in response', (done) => {
-            request.get(base, (err, res, body) => {
-                expect(res.body).toContain('polo');
-                done()
-            })
-        })
-    })
+    }); 
 });
