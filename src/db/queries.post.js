77dfd2e0f01e6ddb59a9fe59e607c237bb3ca768
Dianna Sokhan <diannasokhan@gmail.com>
2019-08-04 22:55:23 -0400

    Adding Flair feature

--- src/db/queries.post.js
+++ src/db/queries.post.js
@@ -1,5 +1,5 @@
 const Post = require('./models').Post;
-const Topic = require('./models').Topic;
+const Flair = require('./models').Flair;
 
 module.exports = {
     addPost(newPost, callback){
@@ -11,7 +11,12 @@ module.exports = {
         })
     },
     getPost(id, callback){
-        return Post.findById(id)
+        return Post.findById(id, {
+            include: [{
+                model: Flair,
+                as: 'flairs'
+            }]
+        })
         .then((post) => {
             callback(null, post);
         }).catch((err) => {
