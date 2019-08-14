3a19ea38e3760a4ed55764b3bd380da7a5bb59c5
Dianna Sokhan <diannasokhan@gmail.com>
2019-08-14 13:21:42 -0400

    posts authorization spec fixes

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -9,6 +9,6 @@ router.post('/topics/:topicId/posts/create',helper.ensureAuthenticated, validati
 router.get('/topics/:topicId/posts/:id', postController.show);
 router.post('/topics/:topicId/posts/:id/destroy', postController.destroy);
 router.get('/topics/:topicId/posts/:id/edit', postController.edit);
-router.post('/topics/:topicId/posts/:id/update', validation.validatePosts,postController.update);
+router.post('/topics/:topicId/posts/:id/update', validation.validatePosts, postController.update);
 
 module.exports = router;
